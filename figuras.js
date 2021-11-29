422d9ffe9de125e685528bc55b785b8b898ffa4c
fbonilla02 <franciscobonillac@gmail.com>
2021-11-29 12:37:19 -0500

    Agregamos las funciones para interactuar con el HTML, ya se puede calcular el area y perimetro de un cuadrado y un triangulo equilátero

--- figuras.js
+++ figuras.js
@@ -6,20 +6,20 @@ function perimetroCuadrado(lado){
 function areaCuadrado(lado){
     return lado * lado;
 }
-
+console.groupEnd();
 
 //Codigo del triangulo
 console.group("triangulos")
-function perimetroTriangulo(lado1, lado2, base){
-    return lado1 + lado2 + base;
+function perimetroTriangulo(lado){
+    return lado * 3;
+}
+function areaTriangulo(lado){
+    // return (base * altura) / 2;
+    return Math.sqrt(3)/ 4 * Math.pow(lado,2);
 
 }
-function areaTriangulo(base, altura){
-    return (base * altura) / 2;
-
-}
-
 console.groupEnd();
+
 
 //Codigo del Circulo
 console.group("Circulos")
@@ -31,17 +31,49 @@ function diametroCirculo(radio){
 // //Pi
 const PI = Math.PI;
 console.log("PI es: " + PI );
-
 // //Circunferencia
 function perimetroCirculo (radio){
     const diametro = diametroCirculo(radio);
     return diametro * PI;
 }
-
 // //Area
 function areaCirculo (radio){
     return (radio * radio) * PI;
 }
-
-
 console.groupEnd();
+
+
+//aqui interactuamos con el HTML
+
+//Calcular Cuadrado
+function calcularperimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado")
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado")
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+//Calcular Triangulo equilatero
+function calcularPerimetroTriangulo(){
+    const input = document.getElementById("InputTriangulo")
+    const value = input.value;
+
+    const perimetro = perimetroTriangulo(value);
+    alert(perimetro);
+}
+function calcularAreaTriangulo(){
+    const input = document.getElementById("InputTriangulo")
+    const value = input.value;
+
+    const area = areaTriangulo(value);
+    alert(area);
+}
